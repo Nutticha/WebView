440486d7736021c8d7a39d7471d5b9e55a123d61
Nutticha <nutticha30234@gmail.com>
2022-04-19 22:31:32 +0700

    Initial commit

---
+++ app/src/main/java/com/nutticha/webview/MainActivity.java
@@ -0,0 +1,28 @@
+package com.nutticha.webview;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new MyWebViewClient ());
+        String url = "https://www.google.co.th/";
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(url);
+    }
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request)
+
+        {
+            return super.shouldOverrideUrlLoading(view, request);
+        }
+    }
+}
